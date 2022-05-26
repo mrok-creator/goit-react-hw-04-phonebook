d44639c7d341b22381ec0537d727b42673388340
Kasianenko Oleksandr <sanekkas@gmail.com>
2022-05-26 21:47:38 +0300

    add uasage of hooks

--- src/components/PhoneBook/PhoneBook.jsx
+++ src/components/PhoneBook/PhoneBook.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { nanoid } from 'nanoid';
 
 import ContactForm from './ContactForm';
@@ -7,39 +7,36 @@ import ContactList from './ContactList';
 
 import s from './phoneBook.module.css';
 
-class PhoneBook extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+function PhoneBook() {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
 
-  componentDidMount() {
-    const data = localStorage.getItem('contacts');
-    const contacts = JSON.parse(data);
+  const [filter, setFilter] = useState('');
 
-    if (contacts?.length) {
-      this.setState({
-        contacts,
-      });
-    }
-  }
+  const firstRender = useRef(true);
 
-  componentDidUpdate(prevProps, prevState) {
-    const { contacts } = this.state;
-    if (prevState.contacts !== contacts) {
+  useEffect(() => {
+    if (!firstRender.current) {
       localStorage.setItem('contacts', JSON.stringify(contacts));
     }
-  }
+  }, [contacts]);
 
-  // ? add contacts to state with input value
-  addContacts = data => {
-    // error due to duplicated contacts
-    const { contacts } = this.state;
+  useEffect(() => {
+    if (firstRender.current) {
+      const data = localStorage.getItem('contacts');
+      const res = JSON.parse(data);
+      if (res?.length) {
+        setContacts(res);
+      }
+      firstRender.current = false;
+    }
+  }, []);
+
+  const addContacts = data => {
     const isDuplicated = contacts.find(item => item.name === data.name);
     if (isDuplicated) {
       alert(`${data.name} is already in your Phonebook`);
@@ -47,36 +44,30 @@ class PhoneBook extends Component {
     }
 
     //? add new  contact logic
-    this.setState(prevState => {
-      const { contacts } = prevState;
+    setContacts(prevContacts => {
       const { name, number } = data;
       const newContacts = {
         id: nanoid(),
         name,
         number,
       };
-      return {
-        contacts: [...contacts, newContacts],
-        name: ' ',
-        number: ' ',
-      };
+      return [...prevContacts, newContacts];
     });
   };
 
-  //? change state due to user action
-  changeFilter = e => {
-    this.setState({
-      filter: e.target.value,
-    });
+  const changeFilter = e => {
+    setFilter(e.target.value);
   };
 
-  //? returned contacts to render
-  getFilteredContacts() {
-    const { contacts, filter } = this.state;
+  const deleteContacts = id => {
+    const res = contacts.filter(item => item.id !== id);
+    setContacts(res);
+  };
+
+  const getFilteredContacts = () => {
     if (!filter) {
       return contacts;
     }
-
     const filterRequest = filter.toLowerCase();
     const filteredContacts = contacts.filter(({ name }) => {
       const res = name.toLowerCase().includes(filterRequest);
@@ -84,34 +75,23 @@ class PhoneBook extends Component {
     });
 
     return filteredContacts;
-  }
-
-  //? delete contacts from state
-  deleteContacts = id => {
-    this.setState(({ contacts }) => ({
-      contacts: contacts.filter(item => item.id !== id),
-    }));
   };
 
-  render() {
-    const { filter } = this.state;
-    const filteredContacts = this.getFilteredContacts();
-    return (
-      <div className={s.container}>
-        <h1 className={s.title}>Phonebook</h1>
-        <ContactForm onSubmit={this.addContacts} />
+  return (
+    <div className={s.container}>
+      <h1 className={s.title}>Phonebook</h1>
+      <ContactForm onSubmit={addContacts} />
 
-        <h2 className={s.title}>Contacts</h2>
+      <h2 className={s.title}>Contacts</h2>
 
-        <Filter onChange={this.changeFilter} filter={filter} />
+      <Filter onChange={changeFilter} filter={filter} />
 
-        <ContactList
-          contacts={filteredContacts}
-          deleteContacts={this.deleteContacts}
-        />
-      </div>
-    );
-  }
+      <ContactList
+        contacts={getFilteredContacts()}
+        deleteContacts={deleteContacts}
+      />
+    </div>
+  );
 }
 
 export default PhoneBook;
